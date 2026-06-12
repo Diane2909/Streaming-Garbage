282743806d72c5d6dd44a056f4b9f61ad389c374
StarCatcher3 <john.s.replogle@gmail.com>
2026-06-12 11:50:57 +0200

    Producer Recursive Draft

--- src/main/scala/example/sparkscala/Producer.scala
+++ src/main/scala/example/sparkscala/Producer.scala
@@ -5,44 +5,52 @@ import java.util.Properties
 
 object Producer {
 
+  val props = new Properties()
+
+  props.load(
+    getClass.getClassLoader.getResourceAsStream("app.properties")
+  )
+
+  val spark = SparkSession.builder()
+      .appName(props.getProperty("app.name"))
+      .master(props.getProperty("app.master"))
+      .getOrCreate()
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+  val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+  
+  val frequency = props.getProperty("producer.frequency").toInt
+  val batch = props.getProperty("producer.batch").toInt
+  val loop = props.getProperty("producer.loop", "false").toBoolean
+  val recursive = props.getProperty("producer.recursive", "false").toBoolean
+  val debug = props.getProperty("debug").toBoolean
+
+  val formatter = java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")
+
   def main(args: Array[String]): Unit = {
-
-    val props = new Properties()
-
-    props.load(
-      getClass.getClassLoader.getResourceAsStream("app.properties")
-    )
-
-    val spark = SparkSession.builder()
-        .appName(props.getProperty("app.name"))
-        .master(props.getProperty("app.master"))
-        .getOrCreate()
-
-    spark.sparkContext.setLogLevel("ERROR")
-
-    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
 
     val inputPath = props.getProperty("producer.input.path")
     val outputPath = props.getProperty("producer.output.path")
-    val frequency = props.getProperty("producer.frequency").toInt
-    val batch = props.getProperty("producer.batch").toInt
-    val loop = props.getProperty("producer.loop", "false").toBoolean
-    val debug = props.getProperty("debug").toBoolean
 
-    copyFiles(fs, inputPath, outputPath, frequency, batch, spark.sparkContext.hadoopConfiguration, loop, debug)
+    copyFiles(inputPath, outputPath)
     
     spark.stop()
   }
 
-  def copyFiles(fs: FileSystem, inputPath: String, outputPath: String, frequency: Int, batch: Int, conf: Configuration, loop: Boolean = false, debug: Boolean = false) {
-    fs.listStatus(new Path("data/source")).grouped(batch)
+  def copyFiles(inputPath: String,
+                outputPath: String,
+                topLevel: Boolean = true)
+  {
+    val files = fs.listStatus(new Path(inputPath)).filter(_.isFile)
+    files.grouped(batch)
       .foreach { batch => 
         batch.foreach { status =>
 
           val file = status.getPath
 
           val destination =
-            new Path("data/input", System.currentTimeMillis() + "_" + file.getName)
+            new Path(outputPath, java.time.LocalDateTime.now().format(formatter) + "_" + file.getName)
 
           if (debug) { println(s"Copying File: ${file.getName}") }
 
@@ -50,14 +58,22 @@ object Producer {
             fs, file,          // source file
             fs, destination,   // destination file
             false,             // don't delete source
-            conf
+            spark.sparkContext.hadoopConfiguration
           )
         }
         if (debug) { println("----------- END OF BATCH ------------") }
         Thread.sleep(frequency * 1000)
       }
-    if (loop) {
-      copyFiles(fs, inputPath, outputPath, frequency, batch, conf, loop)
+
+    if (recursive) {
+      val directories = fs.listStatus(new Path(inputPath)).filter(_.isDirectory)
+      directories.foreach { directory =>
+        copyFiles(inputPath + "/" + directory, outputPath + "/" + directory)
+      }
+    }
+    
+    if (loop & topLevel) {
+      copyFiles(inputPath, outputPath)
     }
   }
 
